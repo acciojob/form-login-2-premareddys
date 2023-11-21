d70f35c11238149bdbbbc63ad6848645264b096b
Siddareddygari Prema Reddy <92963246+premareddys@users.noreply.github.com>
2023-11-21 11:48:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const btn=document.getElementById("submit");
+btn.addEventListener("click", function(){
+	alert("First Name:Virat");
+	alert("Last Name:Kohli");
+	alert("Phone Number:9999888877");
+	alert("Email ID: viratkohli@temp.xyz");
+})
